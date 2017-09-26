396310a60fe1e3f1a83cb8decde404ca93b858b3
DaHee Choi <ekgml9904@naver.com>
2017-09-26 15:04:21 +0900

    openstream add line

--- src/java_network/ParseURL.java
+++ src/java_network/ParseURL.java
@@ -1,5 +1,9 @@
 package java_network;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -7,19 +11,30 @@ public class ParseURL {
 	public static void main(String[] args) {
 		URL opinion = null;
 		URL homePage = null;
-		
-		//http://www.dgit.or.kr/SUB/?ctId=2
-		
+
+		// http://www.dgit.or.kr/SUB/?ctId=2
+
 		try {
-			homePage = new URL("http://www.dgit.or.kr:80"); //:80은 생략 가능
-			opinion =  new URL(homePage,"SUB/?ctId=15");
-			System.out.printf("protocal : %s%n",opinion.getProtocol());
-			System.out.printf("host : %s%n",opinion.getHost());
-			System.out.printf("port : %s%n",opinion.getPort());
-			System.out.printf("path : %s%n",opinion.getPath());
-			System.out.printf("filename : %s",opinion.getFile());
+			homePage = new URL("http://www.dgit.or.kr:80"); // :80은 생략 가능
+			opinion = new URL(homePage, "SUB/?ctId=15");
+			System.out.printf("protocal : %s%n", opinion.getProtocol());
+			System.out.printf("host : %s%n", opinion.getHost());
+			System.out.printf("port : %s%n", opinion.getPort());
+			System.out.printf("path : %s%n", opinion.getPath());
+			System.out.printf("filename : %s", opinion.getFile());
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(opinion.openStream()));
+			String line = null;
+
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+			br.close();
 		} catch (MalformedURLException e) {
 			System.err.println("잘못된 URL 주소");
+			e.printStackTrace();
+		} catch (IOException e) {
+
 			e.printStackTrace();
 		}
 	}
